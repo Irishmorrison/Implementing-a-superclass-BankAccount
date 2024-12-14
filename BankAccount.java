c09c98b7b6af8605b89868cc34f9d808c5600262
Irishmorrison <irishmorrison242424@yahoo.com>
2024-12-14 08:31:58 -0700

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,54 @@
+java
+public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private int accountID;
+    private double balance;
+
+    public BankAccount() {
+        this.balance = 0.0;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public void withdrawal(double amount) {
+        this.balance -= amount;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void accountSummary() {
+        System.out.println("Account Summary:");
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("Account ID: " + accountID);
+        System.out.println("Balance: $" + balance);
+    }
+}
